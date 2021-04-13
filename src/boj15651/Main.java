0f69392fe447c8156d7d5e8ae0141fc0c304a689
김지호 <jiho@kakao.com>
2021-04-13 23:22:30 +0900

    SOLVE BOJ 15651 N과 M (3)

---
+++ src/boj15651/Main.java
@@ -0,0 +1,47 @@
+package boj15651;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+class Main {
+
+    private static int N, M;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = System.in.read() - 48; System.in.read();
+        M = System.in.read() - 48;
+
+        char[] res = new char[M << 1];
+        for (int i = 0; i < M; i++)
+            res[(i << 1) + 1] = ' ';
+
+        dfs(0, res, bw);
+
+        bw.close();
+
+    }
+
+    private static void dfs(int cnt, char[] res, BufferedWriter bw) throws IOException {
+
+        if (cnt == M) {
+
+            bw.write(res);
+            bw.newLine();
+            return;
+
+        }
+
+        for (int i = 0; i < N; i++) {
+
+            res[cnt << 1] = (char) (i + 49);
+            dfs(cnt + 1, res, bw);
+
+        }
+
+    }
+
+}
